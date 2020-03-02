f1b6ceadf1583bc99a8c9d0c6d040bb8e9604fb5
Tomasmed18 <tomy-medina@hotmail.com>
2020-03-02 12:58:06 -0300

    Changed rgb2yuv4() for rgb2gray()

--- xf_ip_accel_app.cpp
+++ xf_ip_accel_app.cpp
@@ -13,14 +13,13 @@ void cbd_accel(hls::stream< ap_axiu<24,1,1,1> >& _src,hls::stream< ap_axiu<8,1,1
 #pragma HLS INTERFACE axis register both  port=_src
 #pragma HLS INTERFACE axis register both  port=_dst
 
-	 xf::Mat<XF_8UC3, HEIGHT, WIDTH, NPIX_CDB> imgOutput0(HEIGHT, WIDTH);
+	 xf::Mat<XF_8UC3, HEIGHT, WIDTH, NPIX_CDB> imgOutput0(HEIGHT, WIDTH); //RGB
 	 xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CDB> imgOutput1(HEIGHT, WIDTH);
-	 xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CDB> img_u(HEIGHT, WIDTH);
-	 xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CDB> img_v(HEIGHT, WIDTH);
 	 xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CDB> imgOutput2(HEIGHT, WIDTH);
 	 xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CDB> imgOutput3(HEIGHT, WIDTH);
 	 xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CDB> imgOutput4(HEIGHT, WIDTH);
 	 xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CDB> imgOutput5(HEIGHT, WIDTH);
+	 xf::Mat<XF_8UC3, HEIGHT, WIDTH, NPIX_CDB> imgOutput6(HEIGHT, WIDTH); //RGB
 
 	 ROI roi;
 
@@ -31,18 +30,21 @@ void cbd_accel(hls::stream< ap_axiu<24,1,1,1> >& _src,hls::stream< ap_axiu<8,1,1
 #pragma HLS stream variable=imgOutput3.data dim=1 depth=1
 #pragma HLS stream variable=imgOutput4.data dim=1 depth=1
 #pragma HLS stream variable=imgOutput5.data dim=1 depth=1
+#pragma HLS stream variable=imgOutput6.data dim=1 depth=1
 	#pragma HLS dataflow
 
 
 	xf::AXIvideo2xfMat(_src, imgOutput0);
 
-	//RGB to YUV conversion, to obtain the grayscale image
-	xf::rgb2yuv4<XF_8UC3,XF_8UC1, HEIGHT, WIDTH, XF_NPPC1>(imgOutput0,imgOutput1,img_u,img_v);
+	//RGB to GRAY conversion, to obtain the grayscale image
+	xf::rgb2gray<XF_8UC3, XF_8UC1, HEIGHT, WIDTH, XF_NPPC1>(imgOutput0, imgOutput1);
 	threshold_accel(imgOutput1, imgOutput2);
 	erosion_accel(imgOutput2, imgOutput3);
 	dilation_accel(imgOutput3, imgOutput4);
 	harris_accel(imgOutput4, imgOutput5);
 	int result = corner_classification(imgOutput5, roi);
+
+
 
 	xf::xfMat2AXIvideo(imgOutput5, _dst);
 
