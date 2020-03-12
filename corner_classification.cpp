14c480db0603ce10a0e8b29509f2941bca4bd3cd
Tomasmed18 <tomy-medina@hotmail.com>
2020-03-12 10:04:28 -0300

    Changed corners[] type to struct Point

--- corner_classification.cpp
+++ corner_classification.cpp
@@ -33,7 +33,7 @@ void draw_corners(xf::Mat<XF_8UC3, HEIGHT, WIDTH, NPIX_CORNER_CLASSIFICATION> & 
 int corner_classification(xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CORNER_CLASSIFICATION> & _src,
 						  xf::Mat<XF_8UC3, HEIGHT, WIDTH, NPIX_CORNER_CLASSIFICATION> & _dst, ROI & roi)
 {
-	ap_uint<COORDINATE_BITS> corners[MAX_CORNERS][2];
+	point corners[MAX_CORNERS];
 	short int cornersCount = 0;
 
 	ap_uint<COORDINATE_BITS> minX = MAX_VAL;
@@ -42,37 +42,26 @@ int corner_classification(xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CORNER_CLASSIFICATIO
 	ap_uint<COORDINATE_BITS> maxY = MAX_VAL;
 
 	for(short int j = 1; j < (_src.rows - 1); j++ ){
-			for(short int i = 10; i < ((_src.cols>>XF_BITSHIFT(XF_NPPC1)) - 10); i++ ){
+			corner_classification_label0:for(short int i = 10; i < ((_src.cols>>XF_BITSHIFT(XF_NPPC1)) - 10); i++ ){
 				//get the pixel brightness value
 				unsigned char pix = _src.data[j*(_src.cols>>XF_BITSHIFT(XF_NPPC1))+i];
 
+				if ((int)pix > 200){ // if its a white pixel
+					//remap the coordinates, deleting radial lens distortion
+					point remapped_intercept = pixel_remap(j, i);
 
-				//remap the coordinates, deleting radial lens distortion
-				point remapped_intercept = pixel_remap(j, i);
-
-				short int y, x;
-				y = remapped_intercept.y;
-				x = remapped_intercept.x;
-
-				if (cornersCount == 500){
-					true;
-				}
-
-				if ((j > 0) && ((int)pix > 200)){
 					//add the coordinates to the array
-					corners[cornersCount][0] = x;
-					corners[cornersCount][1] = y;
-					cornersCount++;
+					corners[cornersCount++] = remapped_intercept;
 
 					//find max and min values
-					if ((minX == MAX_VAL) || (minX > x))
-						minX = x;
-					if ((maxX == MAX_VAL) || (maxX < x))
-						maxX = x;
-					if ((minY == MAX_VAL) || (minY > y))
-						minY = y;
-					if ((maxY == MAX_VAL) || (maxY < y))
-						maxY = y;
+					if ((minX == MAX_VAL) || (minX > remapped_intercept.x))
+						minX = remapped_intercept.x;
+					if ((maxX == MAX_VAL) || (maxX < remapped_intercept.x))
+						maxX = remapped_intercept.x;
+					if ((minY == MAX_VAL) || (minY > remapped_intercept.y))
+						minY = remapped_intercept.y;
+					if ((maxY == MAX_VAL) || (maxY < remapped_intercept.y))
+						maxY = remapped_intercept.y;
 
 				}
 			}
@@ -86,36 +75,36 @@ int corner_classification(xf::Mat<TYPE, HEIGHT, WIDTH, NPIX_CORNER_CLASSIFICATIO
 	for(int i = 0; i < cornersCount; i++){
 		bool correct = false;
 
-		if ((absolute_distance(minX, corners[i][0]) < HANDICAP_CORNER) && (absolute_distance(minY, corners[i][1]) < HANDICAP_CORNER)){
+		if ((absolute_distance(minX, corners[i].x) < HANDICAP_CORNER) && (absolute_distance(minY, corners[i].y) < HANDICAP_CORNER)){
 			v1 = true;
 			correct = true;
 		}
-		else if ((absolute_distance(maxX, corners[i][0]) < HANDICAP_CORNER) && (absolute_distance(minY, corners[i][1]) < HANDICAP_CORNER)){
+		else if ((absolute_distance(maxX, corners[i].x) < HANDICAP_CORNER) && (absolute_distance(minY, corners[i].y) < HANDICAP_CORNER)){
 			v2 = true;
 			correct = true;
 		}
-		else if ((absolute_distance(minX, corners[i][0]) < HANDICAP_CORNER) && (absolute_distance(maxY, corners[i][1]) < HANDICAP_CORNER)){
+		else if ((absolute_distance(minX, corners[i].x) < HANDICAP_CORNER) && (absolute_distance(maxY, corners[i].y) < HANDICAP_CORNER)){
 			v3 = true;
 			correct = true;
 		}
-		else if ((absolute_distance(maxX, corners[i][0]) < HANDICAP_CORNER) && (absolute_distance(maxY, corners[i][1]) < HANDICAP_CORNER)){
+		else if ((absolute_distance(maxX, corners[i].x) < HANDICAP_CORNER) && (absolute_distance(maxY, corners[i].y) < HANDICAP_CORNER)){
 			v4 = true;
 			correct = true;
 		}
 		else{
 			//verify if anyone of the corners if out of the "No error" bounds
-			if (!((absolute_distance(minX, corners[i][0]) < HANDICAP_ERROR)
-				|| (absolute_distance(maxX, corners[i][0]) < HANDICAP_ERROR)
-				|| (absolute_distance(minY, corners[i][1]) < HANDICAP_ERROR)
-				|| (absolute_distance(maxY, corners[i][1]) < HANDICAP_ERROR))){
+			if (!((absolute_distance(minX, corners[i].x) < HANDICAP_ERROR)
+				|| (absolute_distance(maxX, corners[i].x) < HANDICAP_ERROR)
+				|| (absolute_distance(minY, corners[i].y) < HANDICAP_ERROR)
+				|| (absolute_distance(maxY, corners[i].y) < HANDICAP_ERROR))){
 				borderError = true;
-				_dst.data[(corners[i][1])*(_dst.cols>>XF_BITSHIFT(XF_NPPC1)) + (corners[i][0])] = ERROR_BORDER_COLOR;
+				_dst.data[(corners[i].y)*(_dst.cols>>XF_BITSHIFT(XF_NPPC1)) + (corners[i].x)] = ERROR_BORDER_COLOR;
 			} else
-				_dst.data[(corners[i][1])*(_dst.cols>>XF_BITSHIFT(XF_NPPC1)) + (corners[i][0])] = CORRECT_BORDER_COLOR;
+				_dst.data[(corners[i].y)*(_dst.cols>>XF_BITSHIFT(XF_NPPC1)) + (corners[i].x)] = CORRECT_BORDER_COLOR;
 
 		}
 		if (correct)
-			_dst.data[(corners[i][1])*(_dst.cols>>XF_BITSHIFT(XF_NPPC1)) + (corners[i][0])] = CORRECT_CORNER_COLOR;
+			_dst.data[(corners[i].y)*(_dst.cols>>XF_BITSHIFT(XF_NPPC1)) + (corners[i].x)] = CORRECT_CORNER_COLOR;
 	}
 
 	//to return the coordinates of the Region Of Interest
